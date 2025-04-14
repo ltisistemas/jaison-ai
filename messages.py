df78afac4b1928fe943ebcf934c32f43b4cdf33f
LTI Sistemas e Soluções <ltisistemasesolucoes@gmail.com>
2025-04-14 14:44:17 -0300

    V1 -> Falas funcionando

--- messages.py
+++ messages.py
@@ -5,7 +5,11 @@ Responda de forma clara, técnica, mas acessível, e com bom senso de humor quan
 """
 
 FIRST_MESSAGE = "[bold yellow]🤖 Olá, sou o Jaison, seu assistente virtual. Como posso ajudar?[/bold yellow] [bold red](Digite sair para fechar)[/bold red]\n"
+FIRST_MESSAGE_ROBO="Olá, sou o Jaison, seu assistente virtual. Como posso ajudar? Digite sair para fechar"
+
 NEXT_MESSAGES = "[bold yellow]🤖 O que mais posso fazer por você?[/bold yellow] [bold red](Digite sair para fechar)[/bold red]\n"
+NEXT_MESSAGES_ROBO = "O que mais posso fazer por você? Digite sair para fechar"
 
 EXIT_COMMAND = "sair"
 EXIT_MESSAGE = "[bold red]🤖 Encerrando... até logo![/bold red]"
+EXIT_MESSAGE_ROBO = "Encerrando... até logo!"
--- prepare.py
+++ prepare.py
@@ -1,5 +1,6 @@
-from messages import EXIT_COMMAND, FIRST_MESSAGE, NEXT_MESSAGES, SYSTEM_PROMPT
+from messages import EXIT_COMMAND, EXIT_MESSAGE_ROBO, FIRST_MESSAGE, FIRST_MESSAGE_ROBO, NEXT_MESSAGES, NEXT_MESSAGES_ROBO, SYSTEM_PROMPT
 from rich.console import Console
+import pyttsx3
 
 from models_llm.contract_llm import LLMContractInterface
 
@@ -8,14 +9,23 @@ class Prepare:
         self.console = Console()
         self.history = history
         self.history += SYSTEM_PROMPT
+        self.robo = pyttsx3.init()
+        self.robo.setProperty('volume', 1)
+        self.robo.setProperty('rate', 230)
+
+    def falar(self, texto):
+        self.robo.say(texto)
+        self.robo.runAndWait()
 
     def verifica_sair(self):
         if (self.user_input.lower() == EXIT_COMMAND):
             self.console.print("[bold red]🤖 Encerrando... até logo![/bold red]")
+            self.falar(EXIT_MESSAGE_ROBO)
             return False
         return True
 
     def loop(self, llm: LLMContractInterface):
+        self.falar(FIRST_MESSAGE_ROBO)
         self.user_input = self.console.input(FIRST_MESSAGE)
         
         while True:
@@ -26,11 +36,13 @@ class Prepare:
                 # Solicita a resposta da IA e imprime pro usuário
                 response = llm.generate_response(self.user_input)
                 self.console.print(f"[bold green]🤖 Resposta: {response}[/bold green]")
+                self.falar(response)
 
                 # Atualiza histórico
                 self.history += f"\nUsuário: {self.user_input}\nAssistente: {response}"
                 
                 # Inicia o fluxo novamente
+                self.falar(NEXT_MESSAGES_ROBO)
                 self.user_input = self.console.input(NEXT_MESSAGES)
             except Exception as e:
                 self.console.print(f"[bold red]🤖 Erro: {e}[/bold red]")
